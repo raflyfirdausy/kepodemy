9bd12a4e219c23bac18074dbaa070ee6142222a2
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2020-10-02 03:19:28 +0700

    ADD LOG

---
+++ application/backend/logs/log-2020-10-02.php
@@ -0,0 +1,108 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2020-10-02 03:10:59 --> Loader Class Initialized
+INFO - 2020-10-02 03:10:59 --> Helper loaded: url_helper
+INFO - 2020-10-02 03:10:59 --> Helper loaded: form_helper
+INFO - 2020-10-02 03:10:59 --> Helper loaded: array_helper
+INFO - 2020-10-02 03:10:59 --> Helper loaded: download_helper
+INFO - 2020-10-02 03:10:59 --> Helper loaded: inflector_helper
+INFO - 2020-10-02 03:10:59 --> Helper loaded: setting_helper
+INFO - 2020-10-02 03:10:59 --> Helper loaded: waktu_helper
+INFO - 2020-10-02 03:10:59 --> Helper loaded: ultranesia_helper
+INFO - 2020-10-02 03:10:59 --> Helper loaded: utility_helper
+DEBUG - 2020-10-02 03:10:59 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2020-10-02 03:10:59 --> Session: Class initialized using 'files' driver.
+DEBUG - 2020-10-02 03:10:59 --> Encryption: Auto-configured driver 'openssl'.
+INFO - 2020-10-02 03:10:59 --> Encryption: OpenSSL initialized with method AES-128-CBC.
+INFO - 2020-10-02 03:10:59 --> Encryption Class Initialized
+INFO - 2020-10-02 03:10:59 --> Controller Class Initialized
+INFO - 2020-10-02 03:10:59 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\master/pembelajar/v_kelolapembelajar.php
+INFO - 2020-10-02 03:10:59 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/sidebar.php
+INFO - 2020-10-02 03:11:00 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/header.php
+INFO - 2020-10-02 03:11:00 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/footer.php
+INFO - 2020-10-02 03:11:00 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/main.php
+INFO - 2020-10-02 03:11:00 --> Final output sent to browser
+DEBUG - 2020-10-02 03:11:00 --> Total execution time: 0.7209
+INFO - 2020-10-02 03:11:14 --> Loader Class Initialized
+INFO - 2020-10-02 03:11:14 --> Helper loaded: url_helper
+INFO - 2020-10-02 03:11:14 --> Helper loaded: form_helper
+INFO - 2020-10-02 03:11:14 --> Helper loaded: array_helper
+INFO - 2020-10-02 03:11:14 --> Helper loaded: download_helper
+INFO - 2020-10-02 03:11:14 --> Helper loaded: inflector_helper
+INFO - 2020-10-02 03:11:14 --> Helper loaded: setting_helper
+INFO - 2020-10-02 03:11:14 --> Helper loaded: waktu_helper
+INFO - 2020-10-02 03:11:14 --> Helper loaded: ultranesia_helper
+INFO - 2020-10-02 03:11:14 --> Helper loaded: utility_helper
+DEBUG - 2020-10-02 03:11:14 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2020-10-02 03:11:14 --> Session: Class initialized using 'files' driver.
+DEBUG - 2020-10-02 03:11:14 --> Encryption: Auto-configured driver 'openssl'.
+INFO - 2020-10-02 03:11:14 --> Encryption: OpenSSL initialized with method AES-128-CBC.
+INFO - 2020-10-02 03:11:14 --> Encryption Class Initialized
+INFO - 2020-10-02 03:11:14 --> Controller Class Initialized
+INFO - 2020-10-02 03:11:14 --> MY_Model class loaded
+INFO - 2020-10-02 03:11:14 --> Database Driver Class Initialized
+INFO - 2020-10-02 03:11:14 --> Model "Produk_model" initialized
+INFO - 2020-10-02 03:11:14 --> Model "Pengajar_model" initialized
+INFO - 2020-10-02 03:11:15 --> Model "Kategori_model" initialized
+INFO - 2020-10-02 03:11:15 --> Model "ProdukKategori_model" initialized
+INFO - 2020-10-02 03:11:15 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\master/kelas/v_kelas.php
+INFO - 2020-10-02 03:11:15 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/sidebar.php
+INFO - 2020-10-02 03:11:15 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/header.php
+INFO - 2020-10-02 03:11:15 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/footer.php
+INFO - 2020-10-02 03:11:15 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/main.php
+INFO - 2020-10-02 03:11:15 --> Final output sent to browser
+DEBUG - 2020-10-02 03:11:15 --> Total execution time: 0.7292
+INFO - 2020-10-02 03:11:15 --> Loader Class Initialized
+INFO - 2020-10-02 03:11:15 --> Helper loaded: url_helper
+INFO - 2020-10-02 03:11:15 --> Helper loaded: form_helper
+INFO - 2020-10-02 03:11:15 --> Helper loaded: array_helper
+INFO - 2020-10-02 03:11:15 --> Helper loaded: download_helper
+INFO - 2020-10-02 03:11:15 --> Helper loaded: inflector_helper
+INFO - 2020-10-02 03:11:15 --> Helper loaded: setting_helper
+INFO - 2020-10-02 03:11:15 --> Helper loaded: waktu_helper
+INFO - 2020-10-02 03:11:15 --> Helper loaded: ultranesia_helper
+INFO - 2020-10-02 03:11:15 --> Helper loaded: utility_helper
+DEBUG - 2020-10-02 03:11:15 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2020-10-02 03:11:15 --> Session: Class initialized using 'files' driver.
+DEBUG - 2020-10-02 03:11:15 --> Encryption: Auto-configured driver 'openssl'.
+INFO - 2020-10-02 03:11:15 --> Encryption: OpenSSL initialized with method AES-128-CBC.
+INFO - 2020-10-02 03:11:15 --> Encryption Class Initialized
+INFO - 2020-10-02 03:11:15 --> Controller Class Initialized
+INFO - 2020-10-02 03:11:15 --> MY_Model class loaded
+INFO - 2020-10-02 03:11:15 --> Database Driver Class Initialized
+INFO - 2020-10-02 03:11:15 --> Model "Produk_model" initialized
+INFO - 2020-10-02 03:11:15 --> Model "Pengajar_model" initialized
+INFO - 2020-10-02 03:11:15 --> Model "Kategori_model" initialized
+INFO - 2020-10-02 03:11:15 --> Model "ProdukKategori_model" initialized
+INFO - 2020-10-02 03:11:15 --> Model "Pengajar_model" initialized
+INFO - 2020-10-02 03:11:15 --> Final output sent to browser
+DEBUG - 2020-10-02 03:11:15 --> Total execution time: 0.3903
+INFO - 2020-10-02 03:11:18 --> Loader Class Initialized
+INFO - 2020-10-02 03:11:18 --> Helper loaded: url_helper
+INFO - 2020-10-02 03:11:18 --> Helper loaded: form_helper
+INFO - 2020-10-02 03:11:18 --> Helper loaded: array_helper
+INFO - 2020-10-02 03:11:18 --> Helper loaded: download_helper
+INFO - 2020-10-02 03:11:18 --> Helper loaded: inflector_helper
+INFO - 2020-10-02 03:11:18 --> Helper loaded: setting_helper
+INFO - 2020-10-02 03:11:18 --> Helper loaded: waktu_helper
+INFO - 2020-10-02 03:11:18 --> Helper loaded: ultranesia_helper
+INFO - 2020-10-02 03:11:18 --> Helper loaded: utility_helper
+DEBUG - 2020-10-02 03:11:18 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2020-10-02 03:11:18 --> Session: Class initialized using 'files' driver.
+DEBUG - 2020-10-02 03:11:18 --> Encryption: Auto-configured driver 'openssl'.
+INFO - 2020-10-02 03:11:18 --> Encryption: OpenSSL initialized with method AES-128-CBC.
+INFO - 2020-10-02 03:11:18 --> Encryption Class Initialized
+INFO - 2020-10-02 03:11:18 --> Controller Class Initialized
+INFO - 2020-10-02 03:11:18 --> MY_Model class loaded
+INFO - 2020-10-02 03:11:18 --> Database Driver Class Initialized
+INFO - 2020-10-02 03:11:18 --> Model "Produk_model" initialized
+INFO - 2020-10-02 03:11:18 --> Model "Pengajar_model" initialized
+INFO - 2020-10-02 03:11:18 --> Model "Kategori_model" initialized
+INFO - 2020-10-02 03:11:18 --> Model "ProdukKategori_model" initialized
+INFO - 2020-10-02 03:11:18 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\master/kelas/v_tambahkelas.php
+INFO - 2020-10-02 03:11:18 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/sidebar.php
+INFO - 2020-10-02 03:11:18 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/header.php
+INFO - 2020-10-02 03:11:18 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/footer.php
+INFO - 2020-10-02 03:11:18 --> File loaded: C:\xampp\htdocs\kepodemy\application\backend\views\template/main.php
+INFO - 2020-10-02 03:11:18 --> Final output sent to browser
+DEBUG - 2020-10-02 03:11:18 --> Total execution time: 0.3515
