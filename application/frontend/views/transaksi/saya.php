985f10db2d370fdd7782e28c6522044acf0d4b99
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2020-11-08 14:17:52 +0700

    Done Produk Saya

--- application/frontend/views/transaksi/saya.php
+++ application/frontend/views/transaksi/saya.php
@@ -17,32 +17,48 @@
                         Daftar
                     </div>
                     <div class="card-body table-responsive">
-                        <table id="mytable" class="table table-striped table-bordered display responsive nowrap" width="100%">
+                        <table id="mytable" class="table table-striped table-bordered display responsive" width="100%">
                             <thead>
                                 <tr>
                                     <th style="width: 5%;">No</th>
-                                    <th style="width: 10%;">Kode Pembelian</th>
+                                    <th style="width: 12%;">Kode Pembelian</th>
                                     <th>Produk</th>
                                     <th style="width: 15%;">Total</th>
-                                    <th style="width: 15%;">Aksi</th>
+                                    <th style="width: 15%;">Status</th>
+                                    <th style="width: 10%;">Aksi</th>
                                 </tr>
                             </thead>
-                            <tbody>                                
+                            <tbody>
                                 <?php $no = 1;
                                 foreach ($transaksi as $tr) :
                                     $produk = "";
                                     $hargaTotal = 0;
                                     foreach ($tr["transaksidetail"] as $pr) :
                                         $hargaTotal += ($pr->harga - $pr->harga_diskon);
-                                        $produk .= $pr->produk->nama . ", ";
+                                        $produk .= "<a href='" . base_url("kelas/" . $pr->produk->slug) . "' target='_blank'>" . $pr->produk->nama . ", </a>";
                                     endforeach
                                 ?>
                                     <tr>
                                         <td class="text-center"><?= $no++ ?></td>
-                                        <td><?= $tr["kode_transaksi"] ?></td>                                        
+                                        <td><?= $tr["kode_transaksi"] ?></td>
                                         <td><?= $produk ?></td>
                                         <td>Rp <?= Rupiah3($hargaTotal) ?></td>
-                                        <td>aw</td>
+                                        <td>
+                                            <?php
+                                            if ($tr["status_bayar"] == 1) {
+                                                echo '<h5 style="font-size: 100%;" class="badge badge-success">Sukses</h5>';
+                                            } else if ($tr["status_bayar"] == 2) {
+                                                echo '<h5 style="font-size: 100%;" class="badge badge-info">Menunggu Admin</h5>';
+                                            } else if ($tr["status_bayar"] == 3) {
+                                                echo '<h5 style="font-size: 100%;" class="badge badge-danger">Di Tolak</h5>';
+                                            } else {
+                                                echo '<h5 style="font-size: 100%;" class="badge badge-dark">Tidak diketahui</h5>';
+                                            }
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <a href="<?= base_url("transaksi/detail/" . $tr["kode_transaksi"]) ?>" class="btn btn-sm btn-primary col-12">Detail</a>
+                                        </td>
                                     </tr>
                                 <?php endforeach ?>
                             </tbody>
